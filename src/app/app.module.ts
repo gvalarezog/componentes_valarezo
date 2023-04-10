8ae7d046b974a7b2edd44d0d2decc46d109164b9
Guillermo Valarezo Guzman <gvalarezog@gmail.com>
2023-04-10 18:49:46 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    PageWrapperComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
